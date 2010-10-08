12b3e67d1748e27839c836d6b964fe47856d09ac
Nikodemus Siivola <nikodemus@random-state.net>
2010-10-08 13:09:25 +0300

    collect real-time and GC run-time information too
     Use both real-time and run-time information in estimating
     the quality.
    
     Change the result format a bit.

--- bench.lisp
+++ bench.lisp
@@ -70,11 +70,16 @@
            (iterations (or iterations
                            (estimate-iterations-for-run-time fun arguments seconds)))
            (run-arguments (cons iterations arguments))
-           (samples (make-array runs :element-type 'fixnum))
+           (run-time (make-array runs :element-type 'fixnum))
+           (gc-run-time (make-array runs :element-type 'fixnum))
+           (real-time (make-array runs :element-type 'fixnum))
            (consed (make-array runs :element-type 'fixnum))
            (run 0))
       (declare (fixnum run runs))
-      (declare (sb-int:truly-dynamic-extent samples consed run-arguments))
+      ;; TRULY-DYNAMIC-EXTENT because the compiler cannot prove that sample vectors will
+      ;; fit on one page each.
+      (declare (sb-int:truly-dynamic-extent
+                run-time gc-run-time real-time consed run-arguments))
       (unless (typep iterations `(integer 1 ,most-positive-fixnum))
         (error "Number of iterations must be a positive fixnum."))
       (unless (typep runs `(integer 1 ,most-positive-fixnum))
@@ -82,26 +87,47 @@
       (flet ((time-run (&key
                         user-run-time-us
                         system-run-time-us
+                        gc-run-time-ms
+                        real-time-ms
                         bytes-consed
                         &allow-other-keys)
                (declare (fixnum user-run-time-us system-run-time-us))
-               (setf (aref samples run) (+ user-run-time-us system-run-time-us)
+               (setf (aref run-time run) (+ user-run-time-us system-run-time-us)
+                     (aref gc-run-time run) gc-run-time-ms
+                     (aref real-time run) real-time-ms
                      (aref consed run) bytes-consed)))
         (declare (dynamic-extent #'time-run))
         (gc :full t)
         (loop repeat runs
               do (apply #'call-with-timing #'time-run fun run-arguments)
-                 (incf run)))
-      (let* ((mean (mean samples))
-             (std-deviation (standard-deviation samples)))
+              (incf run)))
+      (let ((run-time-mean (mean run-time))
+            (run-time-sdev (standard-deviation run-time))
+            (real-time-mean (mean real-time))
+            (real-time-sdev (standard-deviation real-time)))
         (list name
-              :runs runs
-              :iterations/run iterations
-              :mean-run-time (/ mean 1000000.0)
-              :standard-deviation (/ std-deviation 1000000.0)
-              :bytes-consed (round (mean consed))
-              :quality (when (>= runs 10)
-                         (round (- 100 (* 100.0 (/ std-deviation mean))))))))))
+             :runs runs
+             :iterations/run iterations
+             :run-time
+             (list (/ run-time-mean (expt 10.0 6))
+                   (/ run-time-sdev (expt 10.0 6)))
+             :gc-run-time
+             (list (/ (mean gc-run-time) 1000.0)
+                   (/ (standard-deviation gc-run-time) 1000.0))
+             :real-time
+             (list (/ real-time-mean 1000.0)
+                   (/ real-time-sdev 1000.0))
+             :bytes-consed
+             (list (round (mean consed))
+                   (float (standard-deviation consed)))
+             :quality (when (>= runs 10)
+                        (if (or (zerop run-time-mean) (zerop real-time-mean))
+                            0
+                            (let ((run-time-q (/ run-time-sdev run-time-mean))
+                                  (real-time-q (/ real-time-sdev real-time-mean)))
+                              (max 0
+                                  (round
+                                   (- 100 (* 50.0 (+ run-time-q real-time-q)))))))))))))
 
 (defun run-benchmarks (&key (runs 10) seconds)
   (let (results)
